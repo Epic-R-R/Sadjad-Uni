fe5e7cf504a6914f8881d923152f530050ce47cd
Epic-R-R <epicrr001@gmail.com>
2021-04-27 17:00:15 +0430

    add Validator URL

--- validator.py
+++ validator.py
@@ -9,3 +9,13 @@ class EmptyValidator(Validator):
             raise ValidationError(
                 message="You can't leave this blank", cursor_position=len(value.text)
             )
+
+def UrlValidator(url):
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return url is not None and regex.search(url)
