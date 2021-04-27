fb9a4d0f5cc53f27a0128bb979487fe192296cd1
Epic-R-R <epicrr001@gmail.com>
2021-04-27 16:45:04 +0430

    add screencls function

---
+++ helper.py
@@ -0,0 +1,7 @@
+import platform
+
+def screencls():
+    if platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system("clear")
+    if platform.system() == "Windows":
+        os.system("cls")
