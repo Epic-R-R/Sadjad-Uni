98f98601e9768cabfd74fcda94ba5b05e89f8127
Epic-R-R <epicrr001@gmail.com>
2021-04-27 18:43:05 +0430

    rm download function

--- download.py
+++ download.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 def finder_url(link):
     st = link.find("url")
     fst = st + 4
@@ -9,18 +8,5 @@ def finder_url(link):
             active = False
             continue
         st += 1
-
     complete = link[fst:st]
-    print(complete)
     return(complete)
-    
-def download(username, password, link):
-    driver = webdriver.Chrome(
-        executable_path=r'./webdriver/chromedriver')
-    driver.get(link)
-    driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
-    driver.find_element_by_xpath('//*[@id="loginbtn"]').click()
-    url = finder_url(link)
-    driver.get(
-        f"https://vc2.sadjad.ac.ir/{url}/output/class{url}.zip?download=zip")
