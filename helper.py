473573127f827f565c2c8d4800f74e43f5e8e260
Epic-R-R <epicrr001@gmail.com>
2021-04-27 16:59:06 +0430

    add validator url

--- helper.py
+++ helper.py
@@ -1,7 +1,19 @@
 import platform
+import re
 
 def screencls():
     if platform.system() == "Linux" or platform.system() == "Darwin":
         os.system("clear")
     if platform.system() == "Windows":
         os.system("cls")
+
+
+def is_valid_url(url):
+    regex = re.compile(
+        r'^https?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return url is not None and regex.search(url)
