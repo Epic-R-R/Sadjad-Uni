f9eabf9f924c6111c3a1f73d24a4e66b865a9a58
Epic-R-R <epicrr001@gmail.com>
2021-04-27 23:46:25 +0430

    Add back function

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 import platform
 import re
+from styles import style
+from PyInquirer import prompt
+import os
 
 def screencls():
     if platform.system() == "Linux" or platform.system() == "Darwin":
@@ -8,3 +11,15 @@ def screencls():
         os.system("cls")
 
 
+def back():
+    question = [
+        {
+            "type": "list",
+            "name": "back",
+            "message": "choose",
+            "choices": ["Back"]
+        }
+    ]
+    ans = prompt(question, style=style)
+    if ans["back"] == "Back":
+        return True
